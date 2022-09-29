e79cec9e1f45f3506fa63bec7e32bd3fde0820dd
paohui <2054453988@qq.com>
2022-09-29 10:15:06 +0800

    add splite page api

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ mod models;
 mod schema;
 mod utils;
 
-use models::{InsertableUser, Post, Floor};
+use models::{Floor, InsertableUser, Post};
 use utils::generate_salt_and_hash;
 
 use crate::utils::check;
@@ -85,6 +85,7 @@ async fn main() {
         .route("/login", get(login).post(login_post))
         .route("/register", get(register).post(register_post))
         .route("/post/:post_id", get(get_post))
+        .route("/post/:post_id/:page_id", get(get_post_with_page))
         .fallback(get_service(ServeDir::new("./dist")).handle_error(handle_error))
         .layer(Extension((tera, pool)))
         .layer(Extension(sessions_ptr))
@@ -170,14 +171,14 @@ async fn login(
     Ok((StatusCode::OK, header, body))
 }
 
-async fn get_post(
-    Path(post_id): Path<i32>,
-    may_user_id: UserIdFromSession,
+async fn get_post_with_page(
+    Path((post_id, page)): Path<(i32, i32)>,
+    _: UserIdFromSession,
     Extension((tera, pool)): Extension<(Tera, Pool<ConnectionManager<SqliteConnection>>)>,
 ) -> Html<String> {
+    use schema::floor::dsl::{floor, floor_number, post_id as post_id_dsl};
     use schema::posts::dsl::posts;
-    use schema::users::dsl::{users, name};
-    use schema::floor::dsl::{floor, post_id as post_id_dsl};
+    use schema::users::dsl::{name, users};
 
     let post_inner: Post = match posts.find(post_id).first::<Post>(&mut pool.get().unwrap()) {
         Ok(p) => p,
@@ -186,19 +187,81 @@ async fn get_post(
         }
     };
 
-    let post: PostWithAuthor = match users.find(post_inner.author).select(name).first::<String>(&mut pool.get().unwrap()){
-        Ok(author_name) => PostWithAuthor{post: post_inner, author_name: author_name},
-        Err(_) => PostWithAuthor { post: post_inner, author_name: "unknown".to_owned() }
+    let post: PostWithAuthor = match users
+        .find(post_inner.author)
+        .select(name)
+        .first::<String>(&mut pool.get().unwrap())
+    {
+        Ok(author_name) => PostWithAuthor {
+            post: post_inner,
+            author_name: author_name,
+        },
+        Err(_) => PostWithAuthor {
+            post: post_inner,
+            author_name: "unknown".to_owned(),
+        },
     };
 
-    let floors = match floor.filter(post_id_dsl.eq(post.post.id))
-    .get_results::<Floor>(&mut pool.get().unwrap()){
+    let floors = match floor
+        .filter(post_id_dsl.eq(post.post.id))
+        .order_by(floor_number)
+        .limit(10)
+        .offset(((if page < 0 { 0 } else { page - 1 }) * 10).into())
+        .get_results::<Floor>(&mut pool.get().unwrap())
+    {
         Ok(floors) => floors,
         Err(_) => {
             return "can't find floors".to_owned().into();
         }
     };
 
+    let mut context = Context::new();
+    context.insert("post", &post);
+    context.insert("floors", &floors);
+    tera.render("post.html", &context).unwrap().into()
+}
+
+async fn get_post(
+    Path(post_id): Path<i32>,
+    _: UserIdFromSession,
+    Extension((tera, pool)): Extension<(Tera, Pool<ConnectionManager<SqliteConnection>>)>,
+) -> Html<String> {
+    use schema::floor::dsl::{floor, post_id as post_id_dsl};
+    use schema::posts::dsl::posts;
+    use schema::users::dsl::{name, users};
+
+    let post_inner: Post = match posts.find(post_id).first::<Post>(&mut pool.get().unwrap()) {
+        Ok(p) => p,
+        Err(_) => {
+            return "post not found".to_owned().into();
+        }
+    };
+
+    let post: PostWithAuthor = match users
+        .find(post_inner.author)
+        .select(name)
+        .first::<String>(&mut pool.get().unwrap())
+    {
+        Ok(author_name) => PostWithAuthor {
+            post: post_inner,
+            author_name: author_name,
+        },
+        Err(_) => PostWithAuthor {
+            post: post_inner,
+            author_name: "unknown".to_owned(),
+        },
+    };
+
+    let floors = match floor
+        .filter(post_id_dsl.eq(post.post.id))
+        .limit(10)
+        .get_results::<Floor>(&mut pool.get().unwrap())
+    {
+        Ok(floors) => floors,
+        Err(_) => {
+            return "can't find floors".to_owned().into();
+        }
+    };
 
     let mut context = Context::new();
     context.insert("post", &post);
