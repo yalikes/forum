21f1e55a60b30b10852bbe8f2bd1355e3376eb20
paohui <2054453988@qq.com>
2023-04-01 01:21:53 +0800

    setup diesel

--- migrations/2022-08-12-080110_add_posts/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-CREATE TABLE posts(
-        id INTEGER PRIMARY KEY,
-        author INTEGER NOT NULL,
-        title TEXT NOT NULL
-);
--- migrations/2022-09-27-020136_floor_not_null_id/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-DROP TABLE IF EXISTS floor;
-CREATE TABLE floor(
-        id INTEGER PRIMARY KEY NOT NULL,
-        post_id INTEGER NOT NULL,
-        floor_number INTEGER NOT NULL,
-        author INTEGER NOT NULL,
-        content TEXT NOT NULL
-);
